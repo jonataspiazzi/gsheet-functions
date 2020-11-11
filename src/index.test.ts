ea96cdc39249ef828f330f2f0919e5a73ab5bc3f
Jonatas Piazzi <piazzi_7@hotmail.com>
2020-11-11 14:51:07 -0300

    Created a function to order items by dependencies and priority

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,34 @@
-import { greeting } from './index';
+import { GET_ORDER } from './index';
 
-describe("Global testing", () => {
-  it('Should greet', () => {
-    const msg = greeting();
+describe('Global testing', () => {
+  const matrix = [
+    ['ID', 'Description', 'Dependencies', 'Priority'],
+    ['', '', '', ''],
+    ['I', 'I Description', '', 20],
+    ['A', 'A Description', 'B', 1],
+    ['B', 'B Description', 'C', 2],
+    ['C', 'C Description', '', 3],
+    ['D', 'D Description', 'E, F, I, A, J', 4],
+    ['E', 'E Description', '', 5],
+    ['F', 'F Description', '', 6],
+    ['G', 'G Description', 'A', 7],
+    ['H', 'H Description', 'A, C', 0],
+    ['J', 'J Description', '', -1],
+    ['K', 'K Description', '',]
+  ];
 
-    expect(msg).toBe('Hello World!');
+  it('Should get order of all items.', () => {
+    expect(GET_ORDER('Z', matrix, 0, 2, 3)).toBe('');
+    expect(GET_ORDER('K', matrix, 0, 2, 3)).toBe('');
+    expect(GET_ORDER('A', matrix, 0, 2, 3)).toBe(4);
+    expect(GET_ORDER('B', matrix, 0, 2, 3)).toBe(3);
+    expect(GET_ORDER('C', matrix, 0, 2, 3)).toBe(2);
+    expect(GET_ORDER('D', matrix, 0, 2, 3)).toBe(8);
+    expect(GET_ORDER('E', matrix, 0, 2, 3)).toBe(5);
+    expect(GET_ORDER('F', matrix, 0, 2, 3)).toBe(6);
+    expect(GET_ORDER('G', matrix, 0, 2, 3)).toBe(9);
+    expect(GET_ORDER('H', matrix, 0, 2, 3)).toBe('');
+    expect(GET_ORDER('I', matrix, 0, 2, 3)).toBe(7);
+    expect(GET_ORDER('J', matrix, 0, 2, 3)).toBe(1);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,100 @@
-export function greeting() {
-  return "Hello World!";
+interface ActivityItem {
+  id: string;
+  dependenciesNames: string[],
+  priority: number,
+  dependencies: ActivityItem[]
+}
+
+interface Dictionary<T> {
+  [key: string]: T;
+}
+
+function makeActivityItem(row: any[], idColumnIndex: number, dependenciesColumnIndex: number, priorityColumnIndex: number): ActivityItem {
+  return {
+    id: row[idColumnIndex],
+    dependenciesNames: row[dependenciesColumnIndex] ? row[dependenciesColumnIndex].split(/\,\s*/g) : [],
+    priority: Number(row[priorityColumnIndex]) || undefined,
+    dependencies: []
+  };
+}
+
+function toActivityDictionary(items: ActivityItem[]) {
+  const dic = {} as Dictionary<ActivityItem>;
+
+  for (const item of items) {
+    dic[item.id] = item;
+  }
+
+  return dic;
+}
+
+function connectDependencies(items: ActivityItem[], itemsDic: Dictionary<ActivityItem>) {
+  for (const item of items) {
+    for (const dependencyName of item.dependenciesNames) {
+      const itemDep = itemsDic[dependencyName];
+
+      if (!itemDep) continue;
+      if (item.dependencies.indexOf(itemDep) >= 0) continue;
+
+      item.dependencies.push(itemDep);
+    }
+  }
+}
+
+function traverse(item: ActivityItem): ActivityItem[] {
+  if (!item) return [];
+  if (!item.dependencies.length) return [item];
+
+  const items = new Array<ActivityItem>();
+
+  for (const dependency of item.dependencies.sort((left, right) => left.priority - right.priority)) {
+    if (!dependency) continue;
+
+    items.push(...traverse(dependency));
+  }
+
+  items.push(item);
+
+  return items;
+}
+
+/**
+ * Give a number representing an order of a specific item identified by `id`.
+ * @param id Id of the item to find order index.
+ * @param matrix Two dimensional array with columns of `id`, `dependencies`, `priority`
+ * @param idColumnIndex Zero based index of the column in `matrix` that contains the `id` information.
+ * @param dependenciesColumnIndex Zero based index of the column in `matrix` that contains the `dependencies` information.
+ * @param priorityColumnIndex Zero based index of the column in `matrix` that contains the `priorities` information.
+ */
+export function GET_ORDER(id: string, matrix: Array<Array<any>>, idColumnIndex: number, dependenciesColumnIndex: number, priorityColumnIndex: number) {
+  if (!id) return "";
+  if (!matrix) return "";
+  if (isNaN(Number(idColumnIndex))) return "";
+  if (isNaN(Number(dependenciesColumnIndex))) return "";
+  if (isNaN(Number(priorityColumnIndex))) return "";
+
+  const items = matrix
+    .map(row => makeActivityItem(row, idColumnIndex, dependenciesColumnIndex, priorityColumnIndex))
+    .filter(item => item.id && !isNaN(Number(item.priority)));
+
+  const currentItem = items.find(i => i.id === id);
+  if (!currentItem) return "";
+
+  const itemsDic = toActivityDictionary(items);
+
+  connectDependencies(items, itemsDic);
+
+  const ROOT_ID = '##ROOT';
+
+  const result = traverse({ id: ROOT_ID, dependencies: items } as ActivityItem);
+  result.splice(result.findIndex(r => r.id === ROOT_ID), 1);
+
+  const reduced = result.reduce((ac, i) => {
+    if (ac.indexOf(i) < 0) {
+      ac.push(i);
+    }
+    return ac;
+  }, new Array<ActivityItem>());
+
+  return reduced.findIndex(item => item.id === id) + 1;
 }
